c4e8763a4e10176b69c94b66a316ad84ec1009a4
Oksana Patsii <oksanapatsii@gmail.com>
2023-10-28 15:23:51 +0200

    add footer

--- src/components/Futer/Futer.tsx
+++ src/components/Futer/Futer.tsx
@@ -1,10 +1,83 @@
+import React from 'react'
 import styles from './Futer.module.css';
+import mapIcon from './img/map.svg' 
+import phone  from './img/phone.svg'
+import instagram from './img/instagram.svg'
+import facebook from './img/facebook.svg'
+import envelope from './img/envelope.svg'
+import youtube from './img/youtube.svg'
+
+
 
 const Futer = (): JSX.Element => {
     return(
-        <div className={styles.ft}>
-            <h1>View Futer</h1>
-        </div>
+        <footer className={styles.siteFooter}>
+            <div className={styles.container}>
+                <div className={styles.footerLeft}>
+                    <a className={styles.ftLogo} href="/"><h1>Petscare</h1></a>
+                    <div className={styles.ftInfo}>
+                        <div className={styles.ftInfoItem}>
+                            
+                        <span className={styles.ftInfoIcon} >
+                        <img src={mapIcon} alt="" className={styles.ic} />
+                        </span>
+                        <span>Germany, Berlin</span>
+                        </div>
+
+                        <div className={styles.ftInfoItem}>
+                        <span className={styles.ftInfoIcon} >
+                        <img src={phone} alt="" className={styles.ic} />
+                        </span>
+                        <span>+49 175 9587225</span>
+                        </div>
+
+                        <div className={styles.ftInfoItem}>
+                        <span className={styles.ftInfoIcon} >
+                        <img src={envelope} alt="" className={styles.ic} />
+                        </span>
+                        <span>Petscare@gmail.com</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.ftSocial}>
+                    <a href="#"><img src={facebook} alt="" className={styles.ic} /></a>
+                    <a href="#"><img src={instagram} alt="" className={styles.ic} /></a>
+                    <a href="#"><img src={youtube} alt="" className={styles.ic}/></a>
+                    </div>
+                </div>
+                <div className={styles.ftRight}>
+                    <div className={styles.ftMenu}>
+                        <div className={styles.ftTitle}>Unsere Diensleistungen</div>
+                        <a href="#">Verkauf von Hunde</a>
+                        <a href="#">Hotel für Hunde</a>
+                        <a href="#">Hundeverpaarrung</a>
+                        <a href="#">Hundesitter</a>
+                        
+                        </div>
+                        <div className={styles.ftMenu}>
+                        <div className={styles.ftTitle}>Website-Regeln</div>
+                        <a href="#">Datenschutzbestimmungen</a>
+                        <a href="#">Hotel für Hunde</a>
+                        <a href="#">Hundeverpaarrung</a>
+                        <a href="#">Nutzungsvertrag</a>
+                        <a href="#">Zahlung</a>
+                        
+                        </div>
+                        <div className={styles.ftMenu}>
+                        <div className={styles.ftTitle}>Hilfe und Unterstützung</div>
+                        <a href="#">Datenschutzbestimmungen</a>
+                        <a href="#">Registriren</a>
+                        <a href="#">Eine fragen stelen</a>
+                    </div>
+                    
+                </div>
+            </div>
+                                    <div className={styles.ftCopyRight}>
+                        <p>
+                        2023 &copy; Petscare alle Rechte vorbehalten 
+                        </p>
+                    </div>
+        </footer>
     )
 }
 export default Futer
