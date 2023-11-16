73fd1125fea814e0e34bee247afe14e393dcb5f5
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-11-17 00:29:07 +0100

    add admin

---
+++ src/components/Pages/AdminRoom/Users/UsersOperatons.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+
+//import s from '../.Admin.module.css';
+
+import styles from '../Admin.module.css';
+
+const UsersOperatons = (): JSX.Element => {
+	return (
+		<div className={styles.cont}>
+			<h1>Операции с юзерами</h1>
+		</div>
+	);
+};
+export default UsersOperatons;
--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -2,13 +2,25 @@
 import Carousels from './Carousels/Carousels';
 import Contents from './Content/Contents';
 import Sitters from '../Sitters/Sitters';
+import { useAppSelector } from '../../../app/hooks';
+import { selectUser } from '../../../features/auth/selectors';
+import Admin from '../AdminRoom/Admin';
 
 // eslint-disable-next-line prettier/prettier
 const Home = (): JSX.Element => {
+	const user = useAppSelector(selectUser);
+
 	return (
 		<div>
-			<Carousels />
-			<Sitters />
+			{user?.role === 'ADMIN' ? (
+				<Admin />
+			) : (
+				<>
+					<Carousels />
+					<Sitters />
+				</>
+			)}
+
 			{/* <Contents /> */}
 		</div>
 	);
