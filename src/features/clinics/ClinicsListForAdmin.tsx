16cc748a0156003373cce15c7da8dc9916459059
Viktoriia Ustymova <svi.3481@gmail.com>
2023-11-17 17:45:29 +0100

    api edited, clinics and kennels for admin added

---
+++ src/features/clinics/ClinicsListForAdmin.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { selectClinics } from './selector';
+import { deleteClinic, loadClinics } from './clinicsSlice';
+import s from '../../components/Pages/Clinics/Clinics.module.css';
+import ClinicEditForm from './ClinicEditForm';
+
+export default function ClinicsListForAdmin(): JSX.Element {
+	const clinics = useAppSelector(selectClinics);
+	const dispatch = useAppDispatch();
+	// const [showList, setShowList] = useState(false);
+	const [page, setPage] = useState(1);
+	const itemsPerPage = 9;
+
+	const handleClick = (): void => {
+		dispatch(loadClinics());
+		// setShowList(true);
+	};
+
+	const startIndex = (page - 1) * itemsPerPage;
+	const endIndex = page * itemsPerPage;
+	const currentClinics = clinics.slice(startIndex, endIndex);
+
+	return (
+		<div className={s.clinicContainer}>
+			<div className={s.btnList}>
+				<button type="submit" onClick={handleClick} className={s.btn}>
+					Show clinic list
+				</button>
+			</div>
+			{/* {showList} */}
+			<ul className={s.clinicsList}>
+				{currentClinics.map((clinic) => (
+					<li key={String(clinic.id)} className={s.kennelItem}>
+						<div className={s.dogName}>{clinic.name}</div>
+						<div>{clinic.description}</div>
+						<div>{clinic.webSite}</div>
+						<div>{clinic.country}</div>
+						<div>{clinic.clinicCity}</div>
+						<div>{clinic.postCode}</div>
+						<div>{clinic.address}</div>
+						<div>{clinic.telephoneNumber}</div>
+						{ <button type="button" onClick={() => dispatch(deleteClinic(clinic.id))}>
+							Delete
+						</button>
+						<ClinicEditForm clinicId={clinic.id} />
+						</button> }
+					</li>
+				))}
+			</ul>
+			<div className={s.pagination}>
+				{Array.from({ length: Math.ceil(clinics.length / itemsPerPage) }).map((_, index) => (
+					<button key={index} onClick={() => setPage(index + 1)}>
+						{index + 1}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+}
--- src/features/clinics/api.ts
+++ src/features/clinics/api.ts
@@ -2,24 +2,24 @@ import Clinic from './types/Clinic';
 import ClinicDto from './types/ClinicDto';
 
 export async function getAll(): Promise<Clinic[]> {
-	const res = await fetch('/api/clinics');
+	const res = await fetch('https://walrus-app-ie6jv.ondigitalocean.app/api/clinics');
 	return res.json();
 }
 
 export async function getByCity(city: string): Promise<Clinic[]> {
-	const res = await fetch(`/api/clinics/byCities?city=${city}`);
+	const res = await fetch(`https://walrus-app-ie6jv.ondigitalocean.app/api/clinics/byCities?city=${city}`);
 	return res.json();
 }
 
 export async function deleteClinic(id: number): Promise<Clinic> {
-	const res = await fetch(`/api/clinics/${id}`, {
+	const res = await fetch(`https://walrus-app-ie6jv.ondigitalocean.app/api/clinics/${id}`, {
 		method: 'DELETE',
 	});
 	return res.json();
 }
 
 export async function createClinic(clinic: ClinicDto): Promise<Clinic> {
-	const res = await fetch(`/api/clinics`, {
+	const res = await fetch(`https://walrus-app-ie6jv.ondigitalocean.app/api/clinics`, {
 		method: 'POST',
 		body: JSON.stringify(clinic),
 		headers: {
@@ -30,7 +30,7 @@ export async function createClinic(clinic: ClinicDto): Promise<Clinic> {
 }
 
 export async function updateClinic(clinic: ClinicDto, id: number): Promise<Clinic> {
-	const res = await fetch(`/api/clinics/${id}`, {
+	const res = await fetch(`https://walrus-app-ie6jv.ondigitalocean.app/api/clinics/${id}`, {
 		method: 'PUT',
 		body: JSON.stringify(clinic),
 		headers: {
