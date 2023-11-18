ebb7b136fbe6b64e9081a1e614d0a7273cd6a1ce
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-11-18 11:59:33 +0100

    add styles

--- src/components/navbar/NavIn.tsx
+++ src/components/navbar/NavIn.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
 import { useCallback } from 'react';
 import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
@@ -62,6 +60,14 @@ function Navin(): JSX.Element {
 							<Nav.Link as={Link} to="/contact">
 								Contact
 							</Nav.Link>
+
+							{/* <NavDropdown title="" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown> */}
 						</Nav>
 						<Login />
 						<Register />
